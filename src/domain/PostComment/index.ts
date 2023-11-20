d8015ce395dd85ed6671c2638d4980a0bb7c0829
Pedro Luiz da Costa <pedroluiz51@gmail.com>
2023-11-20 15:26:53 -0300

    create post comment domain

---
+++ src/domain/PostComment/index.ts
@@ -0,0 +1,7 @@
+export * from './postCommentTypes';
+export * from './postCommentService';
+export {POST_COMMENT_PATH} from './postCommentApi';
+
+export * from './useCases/usePostCommentList';
+export * from './useCases/usePostCommentCreate';
+export * from './useCases/usePostCommentRemove';
