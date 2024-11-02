7cb96ec9293085d535c08d2dbf7e03fa4a01c36b
pedr0luiz <pedroluiz51@gmail.com>
2024-11-02 16:51:58 -0300

    test: post comment screen and test ci config

--- src/domain/PostComment/index.ts
+++ src/domain/PostComment/index.ts
@@ -1,5 +1,8 @@
 export * from './postCommentTypes';
-export * from './useCases/usePostCommentCreate';
+export * from './postCommentService';
+export {POST_COMMENT_PATH} from './postCommentApi';
+
 export * from './useCases/usePostCommentList';
+export * from './useCases/usePostCommentCreate';
 export * from './useCases/usePostCommentRemove';
 export * from './useCases/useIsPostCommentAllowedToDelete';
