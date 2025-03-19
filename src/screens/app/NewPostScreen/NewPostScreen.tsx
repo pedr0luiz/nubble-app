08e925d13e9d7f8665d062652ae8162fb9fc698e
pedr0luiz <pedroluiz51@gmail.com>
2025-03-18 21:45:52 -0300

    new post screen

--- src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  ListRenderItemInfo,
+  Pressable,
+} from 'react-native';
 
-import {Screen, Text} from '@components';
+import {useMultimediaGetPhotos} from '@services';
+
+import {Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
 
+import {Header} from './components/Header';
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const NUM_COLUMNS = 4;
+const ITEM_WIDTH = SCREEN_WIDTH / NUM_COLUMNS;
+
 export function NewPostScreen({}: AppTabScreenProps<'NewPostScreen'>) {
+  const [selectedImage, setSelectedImage] = useState<string>();
+  // const permission = usePermission('photoLibrary');
+  const {photoList, fetchNextPage} = useMultimediaGetPhotos(
+    // permission.status === 'granted',
+    true,
+    setSelectedImage,
+  );
+
+  const flatListRef = useRef<FlatList>(null);
+
+  function onSelectImage(imageUri: string) {
+    setSelectedImage(imageUri);
+    flatListRef.current?.scrollToOffset({offset: 0, animated: true});
+  }
+
+  function renderItem({item}: ListRenderItemInfo<string>) {
+    return (
+      <Pressable onPress={() => onSelectImage(item)}>
+        <Image
+          key={item}
+          source={{uri: item}}
+          style={{width: ITEM_WIDTH, height: ITEM_WIDTH}}
+        />
+      </Pressable>
+    );
+  }
+
   return (
-    <Screen>
-      <Text preset="headingLarge">Teste new post</Text>
+    <Screen canGoBack noPaddingHorizontal title="Novo post">
+      <FlatList
+        ref={flatListRef}
+        numColumns={NUM_COLUMNS}
+        data={photoList}
+        renderItem={renderItem}
+        onEndReached={fetchNextPage}
+        onEndReachedThreshold={0.1}
+        ListHeaderComponent={
+          <Header imageWidth={SCREEN_WIDTH} imageUri={selectedImage} />
+        }
+      />
     </Screen>
   );
 }
--- src/test/jestSetup.ts
+++ src/test/jestSetup.ts
@@ -19,21 +19,21 @@ jest.mock('@react-navigation/native', () => {
   };
 });
 
-// jest.mock('@react-native-camera-roll/camera-roll', () => ({
-//   CameraRoll: {
-//     getPhotos: jest.fn(async () => ({
-//       edges: [
-//         {node: {image: {uri: 'image-1'}}},
-//         {node: {image: {uri: 'image-2'}}},
-//         {node: {image: {uri: 'image-3'}}},
-//       ],
-//     })),
-//   },
-// }));
+jest.mock('@react-native-camera-roll/camera-roll', () => ({
+  CameraRoll: {
+    getPhotos: jest.fn(async () => ({
+      edges: [
+        {node: {image: {uri: 'image-1'}}},
+        {node: {image: {uri: 'image-2'}}},
+        {node: {image: {uri: 'image-3'}}},
+      ],
+    })),
+  },
+}));
 
-// jest.mock('react-native-permissions', () =>
-//   require('react-native-permissions/mock'),
-// );
+jest.mock('react-native-permissions', () =>
+  require('react-native-permissions/mock'),
+);
 
 // jest.mock('../services/permission/permissionService', () => ({
 //   permissionService: {
